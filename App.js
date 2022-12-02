5e16b5e3d225bddd9484fe93d0a27324c3440c38
Jeyabaskar A <Jeyabaskar@increscotech.com>
2022-12-02 22:21:07 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { SafeAreaView, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Edvanz Example</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
